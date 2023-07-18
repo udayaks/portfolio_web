a54e776de682b547c82dbc787545d01cdefd6ab8
Udaya Krishna Shrestha <udaya.shrestha@devfinity.io>
2023-07-18 16:30:12 +0545

    header changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,8 @@ const Header: React.FC = () => {
     <header className="bg-gray-900 text-white py-4">
       <div className="container mx-auto">
         <h1 className="text-2xl font-bold">Udaya Krishna Shrestha</h1>
-        <p>Totally in love with Prinsha Shrestha ❤️</p>
+        <p className='mb-3'>Totally in love with Prinsha Shrestha ❤️</p>
+        <span className=''>Email: shresthaudayakrishna@gmail.com</span>
       </div>
     </header>
   );
